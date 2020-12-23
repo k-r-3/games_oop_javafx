c5f5e7349c2fa379f906998a2e4eaf2a3798519b
k-r-3 <rke19922802@gmail.com>
2020-12-23 22:06:22 +0500

    Сокобан. Тестовое задание. [#433644]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -3,6 +3,46 @@ package ru.job4j.puzzle;
 public class Win {
     public static boolean check(int[][] board) {
         boolean rsl = true;
+        int[] diagonal = extractDiagonal(board);
+        for (int i = 0; i < board.length ; i++) {
+            if (diagonal[i] == 1) {
+                if (checkHorizontal(board, i) || checkVertical(board, i)) {
+                    return rsl;
+                }
+                rsl = false;
+                break;
+            }
+        }
         return rsl;
+    }
+
+    public static boolean checkHorizontal(int[][] board, int row) {
+        boolean result = true;
+        for (int i = row; i < board[row].length ; i++) {
+                if (board[row][i] != 1) {
+                    result = false;
+                    break;
+                }
+        }
+        return result;
+    }
+
+    public static boolean checkVertical(int[][] board, int cell) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][cell] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static int[] extractDiagonal(int[][] board) {
+        int[] result = new int[board.length];
+        for (int i = 0; i < board.length ; i++) {
+            result[i] = board[i][i];
+        }
+        return result;
     }
 }
